e52c5a5bfd943e1196a31a2712f0ed278fb5c5b6
LAPTOP-GJ7SIUV3\SCITMASTER <gioper@naver.com>
2018-04-19 10:39:55 +0900

    썸네일 추가_LJS 2018:04:19

--- src/main/java/com/cooing/www/dy/vo/AlbumWriteVO.java
+++ src/main/java/com/cooing/www/dy/vo/AlbumWriteVO.java
@@ -9,6 +9,7 @@ public class AlbumWriteVO {
 	private String album_editor;	
 	private int album_category;
 	private String album_identifier;
+	private String album_thumbnail;
 	
 	public AlbumWriteVO() {
 		super();
@@ -21,15 +22,14 @@ public class AlbumWriteVO {
 		this.album_identifier = album_identifier;
 	}
 	
-	public AlbumWriteVO(int album_num , String album_writer , String album_name , int album_openrange , String album_contents , String album_editor , int album_category , String album_identifier ){
+	public AlbumWriteVO(int album_num , String album_writer , String album_name , int album_openrange , String album_contents , String album_editor , int album_category , String album_identifier , String album_thumbnail){
 		this(album_num , album_writer , album_openrange , album_identifier);
 		this.album_name = album_name;
 		this.album_contents = album_contents;
 		this.album_editor = album_editor;
 		this.album_category = album_category;		
-	}
-	
-	
+		this.album_thumbnail = album_thumbnail;
+	}	
 
 	public AlbumWriteVO(int album_num, String album_name, String album_contents,
 			int album_category, int album_openrange) {
@@ -105,12 +105,19 @@ public class AlbumWriteVO {
 		this.album_identifier = album_identifier;
 	}
 
+	public String getAlbum_thumbnail() {
+		return album_thumbnail;
+	}
+
+	public void setAlbum_thumbnail(String album_thumbnail) {
+		this.album_thumbnail = album_thumbnail;
+	}
+
 	@Override
 	public String toString() {
 		return "AlbumWriteVO [album_num=" + album_num + ", album_writer=" + album_writer + ", album_name=" + album_name
 				+ ", album_openrange=" + album_openrange + ", album_contents=" + album_contents + ", album_editor="
-				+ album_editor + ", album_category=" + album_category + ", album_identifier=" + album_identifier + "]";
+				+ album_editor + ", album_category=" + album_category + ", album_identifier=" + album_identifier
+				+ ", album_thumbnail=" + album_thumbnail + "]";
 	}
-	
-	
 }
